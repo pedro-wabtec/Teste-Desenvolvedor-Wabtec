e2149d1812a9bb13ceaa4e0d2d58e7d4b61802f6
tiago <tiago.saraiva165@hotmail.com>
2026-05-14 13:13:06 -0300

    feat: adiciona automapper

---
+++ teste-dev-wabtec/Mappings/MappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using teste_dev_wabtec.Api.DTOs;
+using teste_dev_wabtec.Api.Models;
+
+namespace teste_dev_wabtec.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile() 
+        {
+            #region ModelToDto
+            CreateMap<TaskItem, TaskResponse>();
+            #endregion
+        }
+    }
+}
