3aadc1c353baec8197c8ad8dcc6e21a05f926fbe
Pâmela G. <47401012+p4mela-g@users.noreply.github.com>
2026-05-15 23:32:54 -0300

    ajusta endpoint de criação com validação e resposta

--- teste-dev-wabtec/Controllers/TasksController.cs
+++ teste-dev-wabtec/Controllers/TasksController.cs
@@ -21,9 +21,18 @@ public class TasksController : ControllerBase
         throw new NotImplementedException();
     }
 
+    [HttpPost]
+    [ProducesResponseType(typeof(TaskResponse), StatusCodes.Status201Created)]
+    [ProducesResponseType(typeof(string), StatusCodes.Status400BadRequest)]
     public IActionResult CreateTask([FromBody] CreateTaskRequest request)
     {
-        // TODO: implementar Controller CreateTask
-        throw new NotImplementedException();
+        if (string.IsNullOrWhiteSpace(request.Title))
+        {
+            return BadRequest("Task title is required.");
+        }
+
+        var createdTaskResponse = _taskService.Create(request.Title);
+
+        return StatusCode(201, createdTaskResponse);
     }
 }
